999f971b43286bf9d66919fc00f07e53339a8fae
gregwhelan <gregwhelan@gmail.com>
2023-04-09 11:18:37 -0700

    cleanup script that uses import

--- validation/import_from_denoland.js
+++ validation/import_from_denoland.js
@@ -1,19 +1,3 @@
 import * as uuid from "https://deno.land/std@0.182.0/uuid/mod.ts";
 
 console.log(uuid.v1.generate());
-
-function calculatePi() {
-  let i = 1n;
-  let x = 3n * (10n ** 1000020n);
-  let pi = x;
-  while (x > 0) {
-    x = x * i / ((i + 1n) * 4n);
-    pi += x / (i + 2n);
-    i += 2n;
-  }
-console.log("wtf");
-
-  return pi.toString();
-}
-
-console.log(calculatePi());
